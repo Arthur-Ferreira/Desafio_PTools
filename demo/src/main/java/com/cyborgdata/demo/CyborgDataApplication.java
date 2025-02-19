c4d5155aaacb1c5be03728d4e77ff15e2970c717
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-19 10:56:06 -0300

    First commit: initial structure

---
+++ demo/src/main/java/com/cyborgdata/demo/CyborgDataApplication.java
@@ -0,0 +1,13 @@
+package com.cyborgdata.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyborgDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyborgDataApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/cyborgdata/demo/CyborgDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyborgdata.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyborgDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
