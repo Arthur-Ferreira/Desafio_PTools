462bea20a1ce85b202b8be8695645513b5e11a47
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-20 23:56:15 -0300

    Atualizacao da Classe principal para evitar erros ao nao usar um banco de dados

--- demo/src/main/java/com/cyborgdata/demo/CyborgDataApplication.java
+++ demo/src/main/java/com/cyborgdata/demo/CyborgDataApplication.java
@@ -1,10 +1,21 @@
 package com.cyborgdata.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableAsync
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Pending Processing API",
+				version = "1.0",
+				description = "API for handling pending data processing"
+		)
+)
 public class CyborgDataApplication {
 
 	public static void main(String[] args) {
