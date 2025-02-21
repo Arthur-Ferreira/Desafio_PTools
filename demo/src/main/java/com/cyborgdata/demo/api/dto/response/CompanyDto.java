a6cb250a76962b594a5cac1dcd063f27ec418d0e
Arthur Ferreira <arthurfdarosa@gmail.com>
2025-02-20 23:46:06 -0300

    Mapeamento da informações de hash e da empresa

---
+++ demo/src/main/java/com/cyborgdata/demo/api/dto/response/CompanyDto.java
@@ -0,0 +1,17 @@
+package com.cyborgdata.demo.api.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CompanyDto {
+  private String hash;
+  private String company;
+}
